f766095bee8e90119bc3ab9ff000883ced5b4b3f
Korazza <corattiroberto@gmail.com>
2020-08-06 14:14:54 +0200

    Loading pre-trained model

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 import * as tf from '@tensorflow/tfjs';
-import trainingData from './data/training.json';
-import './App.scss';
+import './App.css';
 
 const App = () => {
-	const model = useRef(tf.sequential());
+	const model = useRef(null);
 	const randomColor = () => {
 		return [
 			Math.floor(Math.random() * 255),
@@ -12,45 +11,28 @@ const App = () => {
 			Math.floor(Math.random() * 255),
 		];
 	};
-	const [color, setColor] = useState([0, 0, 0]);
-	const [loss, setLoss] = useState(0);
-	const [accuracy, setAccuracy] = useState(0);
+	const [color, setColor] = useState([255, 255, 255]);
 	const [prediction, setPrediction] = useState(0);
 
 	useEffect(() => {
 		(async () => {
-			model.current.add(
-				tf.layers.dense({ inputShape: [3], units: 6, activation: 'relu' })
-			);
-			model.current.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-			model.current.compile({
-				loss: 'meanSquaredError',
-				optimizer: tf.train.adam(0.05),
-				metrics: ['accuracy'],
-			});
-			await train();
+			model.current = await tf.loadLayersModel('./model/model.json');
+			console.log(model.current);
 			setColor(randomColor());
 		})();
 	}, []);
 
 	useEffect(() => {
-		tf.engine().startScope();
-		const input = tf.tensor2d([color]).div(255.0);
-		const output = model.current.predict(input);
-		setPrediction(output.dataSync());
-		tf.engine().endScope();
+		(async () => {
+			if (!model.current) return;
+			tf.engine().startScope();
+			const input = tf.tensor2d([color]).div(255.0);
+			const output = await model.current.predict(input).data();
+			console.log(output);
+			setPrediction(output);
+			tf.engine().endScope();
+		})();
 	}, [color]);
-
-	const train = async () => {
-		tf.engine().startScope();
-		const epochs = 200;
-		const X = tf.tensor2d(trainingData.map((color) => color.input)).div(255.0);
-		const y = tf.tensor1d(trainingData.map((color) => color.output));
-		const history = (await model.current.fit(X, y, { epochs })).history;
-		setLoss(history.loss[epochs - 1]);
-		setAccuracy(history.acc[epochs - 1]);
-		tf.engine().endScope();
-	};
 
 	const componentToHex = (c) => {
 		var hex = c.toString(16);
@@ -98,8 +80,6 @@ const App = () => {
 				/>
 				<button onClick={() => setColor(randomColor())}>Random Color</button>
 				<div>Prediction: {prediction}</div>
-				<div>Loss: {loss * 100}%</div>
-				<div>Accuracy: {accuracy * 100}%</div>
 			</div>
 		</>
 	);
